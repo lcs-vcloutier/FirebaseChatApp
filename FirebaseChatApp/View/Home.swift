8cc7add13588b369838caf22b8a88ac11fced546
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-20 21:21:11 -0400

    added pod files, xcworkspace, model and view folders

---
+++ FirebaseChatApp/View/Home.swift
@@ -0,0 +1,21 @@
+//
+//  Home.swift
+//  FirebaseChatApp
+//
+//  Created by Cloutier, Vincent on 2020-10-20.
+//
+
+import SwiftUI
+import Firebase
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}

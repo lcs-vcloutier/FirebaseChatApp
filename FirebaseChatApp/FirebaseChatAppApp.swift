7115ee2a36ec1bd0b8580af84d802d12788ca025
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-20 20:20:10 -0400

    Initial Commit

---
+++ FirebaseChatApp/FirebaseChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseChatAppApp.swift
+//  FirebaseChatApp
+//
+//  Created by Cloutier, Vincent on 2020-10-20.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

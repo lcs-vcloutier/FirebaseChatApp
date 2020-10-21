72db7ae4eeab4915a811b789d7d4825c309018b0
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-20 21:26:37 -0400

    moved to right file

--- FirebaseChatApp/View/Home.swift
+++ FirebaseChatApp/View/Home.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import Firebase
 
 struct Home: View {
     var body: some View {
@@ -17,13 +16,5 @@ struct Home: View {
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
         Home()
-    }
-}
-
-class AppDelegate: NSObject,UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        
-        FirebaseApp.configure()
-        return true
     }
 }
